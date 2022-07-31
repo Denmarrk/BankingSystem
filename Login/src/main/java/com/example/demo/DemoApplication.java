9c94f5939e1d9a17695c6fc57ab552c3ca04c713
Denmarrk <gdenmarrk@gmail.com>
2022-07-31 13:04:07 +0800

    test

--- Login/src/main/java/com/example/demo/DemoApplication.java
+++ Login/src/main/java/com/example/demo/DemoApplication.java
@@ -8,9 +8,16 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		
+		//System.out.print("dsadas");
 	}
 //docker image build -t login .
 	//docker container run --name loginfile -p 8081:8081 -d login
 //docker container logs loginfile
+	//g it status  -red
+	// git add .
+	// git commit "message"
+	//git push
+	
 
 }
